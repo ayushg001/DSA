843b75eda82a976c8b42ee465fc514255ffc6449
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-06-25 00:37:58 +0530

    k

---
+++ arrays/p1.java
@@ -0,0 +1,33 @@
+public class p1 {
+    
+
+        public static void fun(int arr[] ){
+            int tp=0 , sum;
+            for( int i =0 ; i<arr.length-1 ; i++){
+
+                for( int j= i ; j<arr.length ; j++){
+                    sum=0;
+                   for( int k = i ; k<=j ; k++){
+
+                        //   System.out.print("(" + arr[i] + "," + arr[j] + ")" );
+                          System.out.print(  arr[k] + "" );
+                          sum =sum + arr[k];
+
+                   } 
+                   System.out.print(" Sum is : " +sum);
+                    System.out.println();
+                }
+                System.out.println();
+            }
+            System.out.println("Number of pairs : " + tp);
+       
+        }
+        
+        public static void main(String [] args) {
+            int arr[] = {2,4,6,8,10};  
+           fun(arr);
+            //    for(int j =0 ; j<arr.length ; j++){
+            //      System.out.print(arr[j] + " ");
+            // }
+        }
+}
