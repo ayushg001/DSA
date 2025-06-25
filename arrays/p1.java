bb6621c7c46bdda58fafcfc7a93e4728b33e7e6c
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-06-26 00:26:34 +0530

    comeback day 3 , wraping the array concept of sigma 4,0 array II

--- arrays/p1.java
+++ arrays/p1.java
@@ -1,40 +1,20 @@
-public class p1 {
-    
+class p1 {
 
-        public static void fun(int arr[] ){
-            int max_sum = Integer.MIN_VALUE , min_sum = Integer.MAX_VALUE, sum;
-            for( int i =0 ; i<arr.length-1 ; i++){
-
-                for( int j= i ; j<arr.length ; j++){
-                    sum=0;
-                    for( int k = i ; k<=j ; k++){
-
-                        //   System.out.print("(" + arr[i] + "," + arr[j] + ")" );
-                          System.out.print(  arr[k] + " " );
-                          sum =sum + arr[k];
-
-                   } 
-                   if(sum> max_sum){
-                    max_sum=sum;
-                   }
-                   if(sum<min_sum){
-                    min_sum=sum;
-                   }
-                   System.out.print(" Sum is : " +sum);
-                    System.out.println();
-                }
-                System.out.println();
-            }
-            System.out.println("Maxi sum is : " + max_sum);
-             System.out.println("Minimum sum is : " + min_sum);
-       
-        }
+    public static void fun(int arr[]){
+        int bp =  Integer.MAX_VALUE ;   //+ infinity
         
-        public static void main(String [] args) {
-            int arr[] = {1,-2,6,-1,3};  
-           fun(arr);
-            //    for(int j =0 ; j<arr.length ; j++){
-            //      System.out.print(arr[j] + " ");
-            // }
-        }
+        int max_profit = 0;
+
+       for( int i = 0 ; i < arr.length ; i ++ ){
+        int sp = arr[i];
+            if( bp < sp ){
+                
+            }
+
+       }
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        fun(arr);
+    }
 }
